53dd9180e54fae0c82a17d377b1d61e9d03ebeab
Grady <grady.li@gmail.com>
2014-10-19 19:38:44 +0800

    Initial draft
    The very basic framework.

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require( "express" );
+var app = express();
+
+app.use( express.static( __dirname + '/public' ) );
+
+app.get( '/', function( req, res ) {
+
+    res.sendFile( __dirname + '/public/app/index.html' );
+} );
+
+// custom 404 page
+app.use( function( req, res ) {
+    res.type( 'text/plain' );
+    res.status( 404 );
+    res.send( '404 - Not Found' );
+} );
+
+// custom 500 page
+app.use( function( err, req, res, next ) {
+    console.error( err.stack );
+    res.type( 'text/plain' );
+    res.status( 500 );
+    res.send( '500 - Server Error' );
+} );
+
+app.set( 'port', process.env.PORT || 3000 );
+
+app.listen( app.get( 'port' ), function() {
+    console.log( 'Express started on http://localhost:' +
+        app.get( 'port' ) + '; press Ctrl-C to terminate.' );
+} );
